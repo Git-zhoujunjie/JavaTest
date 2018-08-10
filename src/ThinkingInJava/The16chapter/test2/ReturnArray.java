582a385df4ad28cded663855a8f87b4104bc7b91
root <1160529743@qq.com>
2018-08-10 11:22:39 +0800

    第十六章 test2

---
+++ src/ThinkingInJava/The16chapter/test2/ReturnArray.java
@@ -0,0 +1,23 @@
+package ThinkingInJava.The16chapter.test2;
+
+import java.util.Arrays;
+
+class BerylliumSphere {
+    private static long count ;
+    private final long id = count++;
+    public String toString() { return "Sphere:" + id ;}
+}
+
+public class ReturnArray {
+    static BerylliumSphere[] spheres(int n) {
+        BerylliumSphere[] results = new BerylliumSphere[n];
+        for(int i = 0;i<3;i++)
+            results[i] = new BerylliumSphere();
+        return results;
+    }
+
+    public static void main(String[] args){
+        BerylliumSphere[] spheresArray = spheres(5);
+        System.out.println(Arrays.toString(spheresArray));
+    }
+}
