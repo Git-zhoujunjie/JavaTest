5c28dfe6fd23d0e9fcb30510ae31cbe6e623ea06
root <1160529743@qq.com>
2018-09-17 17:26:52 +0800

    DataStructureAndAlgorithmAnalysis

--- src/DataStructureAndAlgorithmAnalysis/chapter1/Example1.java
+++ src/DataStructureAndAlgorithmAnalysis/chapter1/Example1.java
@@ -1,4 +1,26 @@
 package DataStructureAndAlgorithmAnalysis.chapter1;
 
+
+import java.util.Comparator;
+
+class CaseInsensitiveCompare implements Comparator<String>{
+    public int compare(String lhs,String rhs){
+        return lhs.compareToIgnoreCase(rhs);
+    }
+}
+
 public class Example1 {
+    public static <AnyType> AnyType findMax(AnyType[] arr, Comparator<? super AnyType> cmp){
+        int maxIndex = 0;
+        for(int i = 1;i<arr.length;i++){
+            if(cmp.compare(arr[i],arr[maxIndex]) > 0)
+                maxIndex = i;
+        }
+        return arr[maxIndex];
+    }
+
+    public static void main(String[] args){
+        String[] arr = {"ZEBRA","zlligator","crocodile"};
+        System.out.println(findMax(arr,new CaseInsensitiveCompare()));
+    }
 }
