dcfd70d667341adcce8f182ad781625dbf55aa14
Zhoujunjie <1160529743@qq.com>
2018-11-22 21:42:27 +0800

    hashTable

---
+++ src/DataStructureAndAlgorithmAnalysis/chapter5/SeperateChainingHashTable.java
@@ -0,0 +1,44 @@
+package DataStructureAndAlgorithmAnalysis.chapter5;
+
+import java.util.List;
+
+public class SeperateChainingHashTable<AnyType> {
+    public SeperateChainingHashTable(){
+
+    }
+    public SeperateChainingHashTable(int size){
+
+    }
+
+    public void insert(AnyType x){
+
+    }
+    public void remove(AnyType x){
+
+    }
+    public void contains(AnyType x){
+
+    }
+    public void makeEmpty(){
+
+    }
+
+    private static final int DEFAULT_TABLE_SIZE = 101;
+
+    private List<AnyType>[] theLists;
+    private int currentSize;
+
+    private void rehash(){
+
+    }
+    private int myHash(AnyType x){
+        return 0;
+    }
+
+    private static int nextPrime(int n){
+        return 0;
+    }
+    private static boolean isPrime(int n){
+        return true;
+    }
+}
